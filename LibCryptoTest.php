06e9fe36ca87b8490d7613a5bae278a8f8ef8600
James Clayton <james.r.clayton@gmail.com>
2013-07-04 21:35:13 -0400

    LibCrypto initial creation

---
+++ LibCryptoTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * LibCryptoTest - Unit test for LibCrypto. 
+ * @author James Clayton <james.r.clayton@gmail.com>
+ * @version 0.0.1
+ * @copyright (c) 2013, James Clayton
+ * @package LibCryptoTest
+ * @license http://opensource.org/licenses/ISC ISC License (ISC)
+ */
+
+include_once('../LibEasyPHP/LibConfig.php');
+
+$html->PrintLine('LibCryptoTest - Unit test for LibCrypto.', 'p');
+
+?>
