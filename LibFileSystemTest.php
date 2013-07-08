cdcf5e566804b0bbc9008548487dc41e43df01ad
James Clayton <james.r.clayton@gmail.com>
2013-07-08 15:32:41 -0400

    Updates

--- LibFileSystemTest.php
+++ LibFileSystemTest.php
@@ -13,4 +13,38 @@ include_once('../LibEasyPHP/LibConfig.php');
 
 $html->PrintLine('LibFileSystemTest - Unit test for LibFileSystem.', 'p');
 
+$HelloWorldFile = 'C:/HelloWorld.txt';
+$HelloWorldMessage = 'Blah blah blah!';
+
+// if(!$fs->FileExists($HelloWorldFile)) {
+// 	$fs->WriteDataToFile($HelloWorldFile, $HelloWorldMessage);
+// }
+
+// if($fs->FileExists($HelloWorldFile)) {
+// 	$NewHelloWorldFile = $fs->ReadFileIntoString($HelloWorldFile);
+// 	$html->PrintLine($NewHelloWorldFile, 'p');
+// 	$html->PrintLine('File was successfully written, and read!', 'p');
+// } else {
+// 	$html->PrintLine('File was not successfully written and/or read!', 'p');
+// }
+
+if($fs->FileExists($HelloWorldFile)) {
+	$html->PrintLine('The file does exist: ' . $HelloWorldFile, 'p');
+} elseif(!$fs->FileExists($HelloWorldFile)) {
+	$html->PrintLine('The file does not exist: ' . $HelloWorldFile, 'p');
+	$fs->WriteDataToFile($HelloWorldFile, $HelloWorldMessage);
+	if($fs->FileExists($HelloWorldFile)) {
+		$html->PrintLine('Originally, this file did not exist. But, it was written: ' . $HelloWorldFile, 'p');
+	} else {
+		$html->PrintLine('This file does not exist. Even after I attempted to write: ' . $HelloWorldFile, 'p');
+	}
+} else {
+	$html->PrintLine('Else. Undecided!', 'p');
+}
+
+if($fs->FileExists($HelloWorldFile)) {
+	$ReadFile = $fs->ReadFileIntoString($HelloWorldFile);
+	$html->PrintLine('Attempting to read file: ' . $ReadFile, 'p');
+}
+
 ?>
